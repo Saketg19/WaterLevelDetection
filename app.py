b81b474835658d22a14542729225fbe155d7288d
Saketg19 <123272261+Saketg19@users.noreply.github.com>
2025-09-15 14:18:35 +0530

    app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ if uploaded_file:
             y_pred = model.predict(X_test)
 
             r2 = r2_score(y_test, y_pred)
-            rmse = np.sqrt(mean_squared_error(y_test, y_pred)) # More robust calculation
+            rmse = np.sqrt(mean_squared_error(y_test, y_pred))
             mae = mean_absolute_error(y_test, y_pred)
 
             # TimeSeries Cross-Validation
